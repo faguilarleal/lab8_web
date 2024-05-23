3c476013a70721781d33b3b0b22c407093d94d43
faguilarleal <102198566+faguilarleal@users.noreply.github.com>
2024-05-23 14:49:11 -0600

    feat(test): initial testing

--- lab8_calculadora/vite.config.js
+++ lab8_calculadora/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  resolve: {
+    '@components': '/src/components'
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './setupTests.js'
+  }
 })
